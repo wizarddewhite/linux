137c70aa00de4fa7afe5eb862cb847f3c80817c6
Wei Yang <richardw.yang@linux.intel.com>
2019-08-25 10:07:13 +0800

    mm/mmap.c: update *next* gap after itself
    Since we link a vma to the leaf of a rb_tree, *next* must be a parent of
    vma if *next* is not NULL. This means if we update *next* gap first, it
    will be re-update again if vma's gap is bigger.
    
    For example, we have a vma tree like this:
    
                    a
                    [0x9000, 0x10000]
                /            \
            b                 c
            [0x8000, 0x9000]  [0x10000, 0x11000]
    
    The gap for each node is:
    
      a's gap = 0x8000
      b's gap = 0x8000
      c's gap = 0x0
    
    Now we want to insert d [0x6000, 0x7000], then the tree look like this:
    
                    a
                    [0x9000, 0x10000]
                /            \
            b                 c
            [0x8000, 0x9000]  [0x10000, 0x11000]
         /
        d
        [0x6000, 0x7000]
    
    b is the *next* of d. If we update b's gap first, it would be 0x1000 and
    propagate to a. And then when update d's gap, which is 0x6000 and
    propagate through b to a again.
    
    If we update d's gap first, the un-consistent gap 0x1000 will not be
    propagated.
    
    Signed-off-by: Wei Yang <richardw.yang@linux.intel.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -590,12 +590,6 @@ static unsigned long count_vma_pages_range(struct mm_struct *mm,
 void __vma_link_rb(struct mm_struct *mm, struct vm_area_struct *vma,
 		struct rb_node **rb_link, struct rb_node *rb_parent)
 {
-	/* Update tracking information for the gap following the new vma. */
-	if (vma->vm_next)
-		vma_gap_update(vma->vm_next);
-	else
-		mm->highest_vm_end = vm_end_gap(vma);
-
 	/*
 	 * vma->vm_prev wasn't known when we followed the rbtree to find the
 	 * correct insertion point for that vma. As a result, we could not
@@ -608,6 +602,13 @@ void __vma_link_rb(struct mm_struct *mm, struct vm_area_struct *vma,
 	rb_link_node(&vma->vm_rb, rb_parent, rb_link);
 	vma->rb_subtree_gap = 0;
 	vma_gap_update(vma);
+
+	/* Update tracking information for the gap following the new vma. */
+	if (vma->vm_next)
+		vma_gap_update(vma->vm_next);
+	else
+		mm->highest_vm_end = vm_end_gap(vma);
+
 	vma_rb_insert(vma, &mm->mm_rb);
 }
 
