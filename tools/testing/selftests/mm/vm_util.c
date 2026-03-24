d4f02c86b766e5e8a3016e9bd001c94d6961dcbd
Chunyu Hu <chuhu@redhat.com>
2026-03-24 09:33:14 +0800

    selftests/mm/vm_util: robust write_file()
    Add two more checks for buflen and numwritten.  The buflen should be at
    least one, otherwise the 'buflen - 1' could underflow and cause trouble. 
    The numwritten should be equal to 'buflen - 1'.  The test will exit if any
    of these conditions aren't met.
    
    Additionally, add more print information when a write failure occurs or
    a truncated write happens, providing clearer diagnostics.
    
    Link: https://lkml.kernel.org/r/20260324013316.2590422-5-chuhu@redhat.com
    Signed-off-by: Chunyu Hu <chuhu@redhat.com>
    Cc: Baolin Wang <baolin.wang@linux.alibaba.com>
    Cc: Barry Song <baohua@kernel.org>
    Cc: David Hildenbrand (Arm) <david@kernel.org>
    Cc: Dev Jain <dev.jain@arm.com>
    Cc: Lance Yang <lance.yang@linux.dev>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Li Wang <liwang@redhat.com>
    Cc: Lorenzo Stoakes (Oracle) <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport (Microsoft) <rppt@kernel.org>
    Cc: Nico Pache <npache@redhat.com>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Cc: Zi Yan <ziy@nvidia.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- tools/testing/selftests/mm/vm_util.c
+++ tools/testing/selftests/mm/vm_util.c
@@ -767,15 +767,23 @@ int unpoison_memory(unsigned long pfn)
 
 void write_file(const char *path, const char *buf, size_t buflen)
 {
-	int fd;
+	int fd, saved_errno;
 	ssize_t numwritten;
+	if (buflen < 1)
+		ksft_exit_fail_msg("Incorrect buffer len: %zu\n", buflen);
 
 	fd = open(path, O_WRONLY);
 	if (fd == -1)
 		ksft_exit_fail_msg("%s open failed: %s\n", path, strerror(errno));
 
 	numwritten = write(fd, buf, buflen - 1);
+	saved_errno = errno;
 	close(fd);
+	errno = saved_errno;
 	if (numwritten < 1)
-		ksft_exit_fail_msg("Write failed\n");
+		ksft_exit_fail_msg("%s write(%s) failed: %s\n", path, buf,
+				strerror(errno));
+	if (numwritten != buflen - 1)
+		ksft_exit_fail_msg("%s write(%s) is truncated, expected %zu bytes, got %zd bytes\n",
+				path, buf, buflen - 1, numwritten);
 }
